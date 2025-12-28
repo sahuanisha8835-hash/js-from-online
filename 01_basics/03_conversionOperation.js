89bb3224eeb6d297ac974bd6cdfe8c5bc9b3b9a2
sahuanisha8835-hash <sahuanisha8835@gmail.com>
2025-12-28 07:38:05 +0000

    various operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -63,3 +63,41 @@ console.log(typeof somenumber);
 let stringNumber= String(somenumber);
 console.log(typeof stringNumber);//string
 console.log(stringNumber);//33
+
+
+
+//************************OPERATION**************************
+
+let value1 = 3;
+let negvalue1 = -value1
+console.log(negvalue1);//-3
+console.log(2+2);//4
+console.log(2-2);//0
+console.log(2*2);//4
+console.log(2**2);//4
+console.log(2/2);//1
+console.log(2%2);//0
+
+let str1 = "Hello"
+let str2 = " Anisha"
+let str3 = str1 + str2
+console.log(str3);//Hello Anisha
+
+//important
+console.log(1+"2");//12
+console.log("1"+2);//12
+console.log("1"+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(true);//true
+console.log(+true);//1
+console.log(+"");//0
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);//101
+
+let gameCounter1 = 100;
+gameCounter1++;
+console.log(gameCounter1);//101
