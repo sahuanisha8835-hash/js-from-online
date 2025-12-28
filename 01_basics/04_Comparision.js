fa4be194118799e2a28ea64509b339f7d455514d
sahuanisha8835-hash <sahuanisha8835@gmail.com>
2025-12-28 14:18:33 +0000

    Comparision in js

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);//true
+console.log(2 >= 1);//true
+console.log(2 < 1);//false
+console.log(2 == 1);//false
+console.log(2 != 1);//true
+
+console.log("2" > 1);//true
+console.log("02" > 1);//true
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+console.log(null < 0);//false
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+//=== check strictly also check the datatype
+
+console.log("2" == 2);//true "" it converts the string data type in to integer type
+console.log("2" === 2);//false 
+
+
